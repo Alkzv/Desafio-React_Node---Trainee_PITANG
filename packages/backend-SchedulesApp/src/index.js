52ab86e75b0ab4a72ce2797c9b6cfc3313c98d47
Alkzv <lucasalkimimtop@hotmail.com>
2022-04-17 20:50:16 -0300

    Criando API
    Criando um modelo padrão para podermos usar posteriormente na criação da nossa API de agendamentos

--- packages/backend-SchedulesApp/src/index.js
+++ packages/backend-SchedulesApp/src/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import { router } from './routes/ScheduleRoute.js'
 
 const app = express()
 
@@ -8,11 +7,34 @@ app.use(express.json())
 
 app.use(cors())
 
-app.use((response, request, next) => {
+app.use((response, request, next) => { // Middleware
   next()
 })
 
-app.use('/api', router)
+app.use(express.json())
+
+app.post('/', (request, response) => {
+  response.status(200).send(`Requisição ${request.method} recebida com sucesso!`)
+})
+
+app.get('/', (request, response) => {
+  response.status(200).send(`Requisição ${request.method} recebida com sucesso!`)
+})
+
+app.get('/:id', (request, response) => {
+  const id = request.params.id
+  response.status(200).send(`Requisição ${request.method} recebida com sucesso! ID:${id}`)
+})
+
+app.put('/:id', (request, response) => {
+  const id = request.params.id
+  response.status(200).send(`Requisição ${request.method} recebida com sucesso! ID: ${id}`)
+})
+
+app.delete('/:id', (request, response) => {
+  const id = request.params.id
+  response.status(200).send(`Requisição ${request.method} recebida com sucesso! ID: ${id}`)
+})
 
 app.listen(3000, () => {
   console.log('Server Running on PORT 3000')
