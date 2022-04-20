13a62eedccb2c38c139274dc8d1228fd72f5e9bd
Alkzv <lucasalkimimtop@hotmail.com>
2022-04-20 17:39:50 -0300

    Criando consultas de verificação para validar a possibilidade de cadastro de agendamentos

--- packages/backend-SchedulesApp/src/controller/ScheduleController.js
+++ packages/backend-SchedulesApp/src/controller/ScheduleController.js
@@ -5,7 +5,7 @@ class ScheduleController extends Controller {
     super('Schedule')
   }
 
-  create (request, response) {
+  async create (request, response) {
     const schema = Joi.object({
       name: Joi.string().min(2).max(255).required(),
       birthDate: Joi.date().raw().required(),
@@ -18,17 +18,69 @@ class ScheduleController extends Controller {
         message: 'invalid data'
       })
     }
+    const checkDayAvailability = await this.checkDayAvailability(request, response)
+    const checkDayTimeAvailability = await this.checkDayTimeAvailability(request, response)
+
+    if (!checkDayAvailability) {
+      return response.status(400).json({ message: 'Unavailable vacancies for this day' })
+    } else if (!checkDayTimeAvailability) {
+      return response.status(400).json({ message: 'Unavailable time for this day' })
+    }
+
     super.create(request, response)
   }
 
-  async getAll (request, response, isOrderByAsc) {
-    console.log(request.body)
+  async getAll (request, response) {
+    const { typeOrderBy } = request.params
     try {
       response.status(200).json(await this.client.findMany({
         orderBy: {
-          schedule: isOrderByAsc ? 'asc' : 'desc'
+          schedule: typeOrderBy
         }
       }))
+    } catch (error) {
+      return response.status(400).json({ message: 'Unsuccessful Listing' })
+    }
+  }
+
+  async checkDayAvailability (request, response) {
+    const { schedule } = request.body
+    const fromDate = schedule.substring(0, 10)
+    const toDate = fromDate.substring(0, 8) + ((parseInt(fromDate.substring(8, 10)) + 1).toString())
+
+    try {
+      const numberSchedulesDay = await this.client.count({
+        where: {
+          schedule: {
+            gte: new Date(fromDate),
+            lt: new Date(toDate)
+          }
+        }
+      })
+      if (numberSchedulesDay < 20) {
+        return true
+      }
+      return false
+    } catch (error) {
+      return response.status(400).json({ message: 'Unsuccessful Listing' })
+    }
+  }
+
+  async checkDayTimeAvailability (request, response) {
+    const { schedule } = request.body
+
+    try {
+      const numberOfSchedulesAtTheSameTimeOfTheDay = await this.client.count({
+        where: {
+          schedule: {
+            equals: schedule
+          }
+        }
+      })
+      if (numberOfSchedulesAtTheSameTimeOfTheDay < 2) {
+        return true
+      }
+      return false
     } catch (error) {
       return response.status(400).json({ message: 'Unsuccessful Listing' })
     }
